282b6e4185c2b3e57e48de7583fc9e9391e2aaba
hansen1416 <badapplesweetie@gmail.com>
2021-12-22 04:55:58 +0800

    1

--- rust-tutorial/src/main.rs
+++ rust-tutorial/src/main.rs
@@ -26,7 +26,49 @@ fn main() {
 
         }
     }
+
+    let database = Database::new().expect("Database::new() crushed");
+
+    // println!("{}", database)
 }
 
+// in struct, add fields,
+struct Database {
+    // field1: String,
+    // field2: u8,
+    map: std::collections::HashMap<String, String>,
+}
 
+// in implementation, add methods
+impl Database {
+
+    fn new() -> Result<Database, std::io::Error> {
+        // read the kv.db file
+        
+        // let content = match std::fs::read_to_string("kv.db") {
+        //     Ok(contents) => {
+        //         contents; // ignore return on the last thing
+        //     }
+        //     Err(error) => {
+        //         return Result::Err(error);
+        //     }
+        // };
+
+        // ? euqal to error handling, if there is an error, return the error
+        let content = std::fs::read_to_string("kv.db")?;
+
+        for line in content.lines() {
+            // ' is char, " is string
+            let (key, value) = line.split_once('\t').expect("Corrupt database");
+
+            println!("key is {}, value is {}", key, value)
+        }
+
+
+        // parse the string
+        Result::Ok(Database {map: std::collections::HashMap::new()})
+    }
+
+
+}
 
