97eeb8a7e56beb29f3a821cf85322c71a2e20869
hansen1416 <badapplesweetie@gmail.com>
2021-12-29 20:01:59 +0800

    1

--- dthread/web/middleware/wallet.ts
+++
@@ -1,9 +0,0 @@
-import { Middleware, Context } from '@nuxt/types';
-import { useConnection, useWallet } from '@solana/wallet-adapter-phantom';
-
-const myMiddleware: Middleware = (context: Context) => {
-  // Use context
-  console.log("wallet middleware");
-}
-
-export default myMiddleware
